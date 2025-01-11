d481eb66810b2ab5c222f522ff794352502d1266
arielz-17 <2300018395@webmail.uad.ac.id>
2025-01-11 18:50:51 +0700

    Update KategoriBuku.java

--- KategoriBuku.java
+++ KategoriBuku.java
@@ -1,9 +1,9 @@
-public class CetakBuku {
-    public void cetakInfo(Buku buku) {
-        System.out.println("Judul: " + buku.getJudul());
-    }
+public class KategoriBuku {
+    public static final String FIKSI = "Fiksi";
+    public static final String NON_FIKSI = "Non-Fiksi";
+    public static final String REFERENSI = "Referensi";
 
-    public void cetakInfo(BukuFiksi bukuFiksi) {
-        System.out.println("Judul: " + bukuFiksi.getJudul() + ", Sub Kategori: " + bukuFiksi.getSubKategori());
-    }
+    public static boolean isValidKategori(String kategori) {
+        return kategori.equals(FIKSI) || kategori.equals(NON_FIKSI) || kategori.equals(REFERENSI);
+    }
 }
