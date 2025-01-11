ed67c03b89453eb2e33e99de0e592f5cdc2c0276
arielz-17 <2300018395@webmail.uad.ac.id>
2025-01-11 18:47:48 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -1,5 +1,32 @@
-public class BukuManager {
-    public void tampilkanDeskripsiBuku(Buku buku) {
-        buku.deskripsiBuku();
+public class Buku extends ItemPerpustakaan {
+    private String ISBN;
+    private String status;
+
+    public Buku(String id, String judul, String pengarang, String ISBN, String kategori) {
+        super(id, judul, pengarang, kategori);
+        if (KategoriBuku.isValidKategori(kategori)) {
+            this.kategori = kategori;
+        } else {
+            throw new IllegalArgumentException("Kategori tidak valid.");
+        }
+        this.ISBN = ISBN;
+        this.status = "tersedia";
     }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("Judul: " + judul + ", Pengarang: " + pengarang + ", ISBN: " + ISBN + ", Kategori: " + kategori + ", Status: " + status);
+    }
 }
