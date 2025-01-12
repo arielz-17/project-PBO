779ba0f92540a6c38c7ff4cfa95a46d84f6d8617
405_Primus Utama Satya <149552122+Pri666mus@users.noreply.github.com>
2025-01-13 00:56:07 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,13 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
+            
             @Override
             public void run() {
                 new PerpustakaanGUI().setVisible(true);
+                
             }
-        });
+        }
+         );
     }
 }
