4c8b93874c861d8f1618c3b407fdf825f300eb28
denaary <cilacapdenaary@gmail.com>
2025-01-13 01:15:05 +0700

    Update AnggotaPerpustakaan.java

--- AnggotaPerpustakaan.java
+++ AnggotaPerpustakaan.java
@@ -2,29 +2,21 @@ abstract class AnggotaPerpustakaan implements Keanggotaan {
     protected String idAnggota;
     protected String nama;
     protected String email;
-    protected String tipeKeanggotaan;
-
-    
+    protected String tipeKeanggotaan;  
     public AnggotaPerpustakaan(String idAnggota, String nama, String email, String tipeKeanggotaan) {
         this.idAnggota = idAnggota;
         this.nama = nama;
         this.email = email;
-        this.tipeKeanggotaan = tipeKeanggotaan;
-        
+        this.tipeKeanggotaan = tipeKeanggotaan;        
     }
 
     public abstract void getInfo();
-
     @Override
     public void daftarKeanggotaan() {
-        System.out.println("Anggota " + nama + " dengan ID " + idAnggota + " telah terdaftar.");
-        
+        System.out.println("Anggota " + nama + " dengan ID " + idAnggota + " telah terdaftar.");   
     }
-
     @Override
     public void hapusKeanggotaan() {
-        System.out.println("Anggota " + nama + " dengan ID " + idAnggota + " telah dihapus.");
-        
+        System.out.println("Anggota " + nama + " dengan ID " + idAnggota + " telah dihapus.");   
     }
-    
 }
