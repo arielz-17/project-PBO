e52d1812da65afb2ca35f590f12255e1a8815034
405_Primus Utama Satya <149552122+Pri666mus@users.noreply.github.com>
2025-01-13 00:58:06 +0700

    Update Siswa.java

--- Siswa.java
+++ Siswa.java
@@ -1,12 +1,1 @@
-public class Siswa extends AnggotaPerpustakaan {
-    public Siswa(String idAnggota, String nama, String email) {
-        super(idAnggota, nama, email, "Siswa");
-    }
 
-    
-    @Override
-    public void getInfo() {
-        System.out.println("Nama: " + nama + ", Nomor Anggota: " + idAnggota + ", Email: " + email + ", Tipe Keanggotaan: " + tipeKeanggotaan);
-    }
-    
-}
