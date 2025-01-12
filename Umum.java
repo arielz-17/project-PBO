8dd3bac2c89c116fa1a6998c757d596339306ae0
405_Primus Utama Satya <149552122+Pri666mus@users.noreply.github.com>
2025-01-13 00:58:29 +0700

    Update Umum.java

--- Umum.java
+++ Umum.java
@@ -1,1 +1,14 @@
+public class Umum extends AnggotaPerpustakaan {
+    public Umum(String idAnggota, String nama, String email) {
+        super(idAnggota, nama, email, "Umum");
+    }
+
+    
+    @Override
+    public void getInfo() {
+        System.out.println("Nama: " + nama + ", Nomor Anggota: " + idAnggota + ", Email: " + email + ", Tipe Keanggotaan: " + tipeKeanggotaan);
+   
+    }
+    
+}
 
