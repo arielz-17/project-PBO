3848d747ff978b5110207181896bf3e390a0a75e
405_Primus Utama Satya <149552122+Pri666mus@users.noreply.github.com>
2025-01-13 00:41:14 +0700

    Create mahasiswa.java

---
+++ mahasiswa.java
@@ -0,0 +1,10 @@
+public class Mahasiswa extends AnggotaPerpustakaan {
+    public Mahasiswa(String idAnggota, String nama, String email) {
+        super(idAnggota, nama, email, "Mahasiswa");
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("Nama: " + nama + ", Nomor Anggota: " + idAnggota + ", Email: " + email + ", Tipe Keanggotaan: " + tipeKeanggotaan);
+    }
+}
